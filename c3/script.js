b34153e55c5b60654376ee2eca4b9595a3a5139c
Rico Sta. Cruz <rico@ambiescent.com>
2012-08-27 11:52:06 +0800

    Update

--- c3/script.js
+++ c3/script.js
@@ -1,3 +1,43 @@
+(function($) {
+  // $("#area").scrollspy({ on: 'tr', onChange: function(){} });
+  $.fn.scrollspy = function(options) {
+    var $this = $(this);
+
+    $this.on('scroll', onscroll);
+    function onscroll() {
+      var y = $this.scrollTop();
+      var percent = y / ($this[0].scrollHeight - $this.height());
+      percent = Math.pow(percent, 3) * 0.9 + 0.1;
+      var current = null;
+      var stop = false;
+
+      $(options.on).each(function() {
+        if (stop) return;
+        var min = $(this).position().top;
+        var isVisible = (min < $this.height() * percent);
+        if (isVisible) {
+          current = this;
+        } else {
+          stop = true;
+        }
+      });
+
+      if ((options.alwaysOn) && (!current)) {
+        current = $($(options.on)[0]);
+      }
+
+      if ($this.data('scroll:current') != current) {
+        $this.data('scroll:current', current);
+        if (typeof options.onChange === 'function') {
+          options.onChange.call($this, current);
+        }
+      }
+    };
+
+    onscroll.call($this);
+    return this;
+  };
+})(jQuery);
 (function() {
 
   function openSidebarTab(href) {
@@ -24,8 +64,28 @@
   $(".files.list .folder").on('click', function() {
     $(this).toggleClass('expanded');
     var ul = $(this).closest('li').find('>ul');
-    console.log(ul);
     ul.slideToggle(200);
+  });
+
+  /* Scrollspy */
+  $(function() {
+   $("#area").scrollspy({
+     on: 'tr.heading',
+     alwaysOn: true,
+     onChange: function(current) {
+       var id = $(current).attr('id');
+       $(".current-page a").removeClass('active');
+
+       $("tr").removeClass('active');
+       $(current).addClass('active');
+
+       if ($(current).find('h1').length > 0) {
+         $(".current-page a[href='#']").addClass('active');
+       } else {
+         $(".current-page a[href='#"+id+"']").addClass('active');
+       }
+     }
+   });
   });
 
 })();
